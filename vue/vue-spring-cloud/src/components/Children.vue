b7bce779451028f60db28ac86d0adab273f9f7c1
xupinyuan <674713418@qq.com>
2019-06-01 20:59:33 +0800

    demo

---
+++ vue/vue-spring-cloud/src/components/Children.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div>父组件传来的消息：{{message}}</div>
+    <div>传递消息给父组件:
+      <input v-model="data"><button v-on:click="sendMessage()">发送</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Children',
+  props:['message'],
+  data(){
+    return{
+      data:''
+    }
+  },
+  methods:{
+    sendMessage(){
+      this.$emit('send',this.data);
+    }
+  }
+}
+</script>
--- vue/vue-spring-cloud/src/views/test.vue
+++ vue/vue-spring-cloud/src/views/test.vue
@@ -1,26 +1,70 @@
 <template>
   <div>
-    <span>{{message}}</span>
-     <span v-for="item in items" >
-       <a v-if="item.username == 1">111</a>
-       {{item.username}}
+    <span>测试取值：{{message}}</span>
+    <div>测试v-if：
+      <span v-if="flag1">true</span>
+      <span v-if="flag2==false">false</span>
+    </div>
+    <div>测试v-for：
+      <span v-for="arr in array" >
+       {{arr}}、
      </span>
+    </div>
+    <div>测试v-on：
+      <button v-on:click="testEvent()">点我点我</button>
+    </div>
+    <HelloWorld></HelloWorld>
+
+    <div>双向绑定测试：<input v-model="name" /><button v-on:click="alertName()">点击弹出值</button></div>
+
+
+    <div>组件消息传递测试
+      <Children  v-bind:message='{name:"消息传递"}' v-on:send="setChidrenData"></Children>
+      <span>子组件传来的消息：{{chidrenData}}</span>
+    </div>
   </div>
 </template>
 
 <script>
+  import HelloWorld from "../components/HelloWorld";
+  import Children from "../components/Children";
+  // 先注册组件
+
   export default {
-    name: "login",
+    name: "test",
+    components: {Children, HelloWorld},
     data() {
       return {
-        message:'测试',
-        items: [
-          {username:'1'},
-          {username:'2'},
-          {username:'3'}
-        ]
+        name:"双向绑定测试",
+        message: '测试取值',
+        //测试v-if
+        flag1 : true,
+        flag2 : false,
+        //测试v-for
+        array: ['1','2','3'],
+        chidrenData:''
       }
-    }
+    },
+    methods:{
+      //测试事件绑定
+      testEvent(){
+        alert('点我点我');
+      },
+      //双向绑定测试
+      alertName(){
+        alert(this.name);
+      },
+      //子组件传递需要通过父组件事件
+      setChidrenData(msg){
+        this.chidrenData = msg
+      }
+    },
+    //钩子函数测试
+    beforeCreate:function () {
+      alert('钩子函数测试');
+    },
+
+
   }
 </script>
 
